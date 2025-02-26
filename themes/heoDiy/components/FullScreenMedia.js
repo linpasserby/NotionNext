f4637195fff1f5f3850f4a2761cbf033ac7aca29
lql <lin_passerby@163.com>
2025-02-26 09:31:35 +0800

    测试4

---
+++ themes/heoDiy/components/FullScreenMedia.js
@@ -0,0 +1,27 @@
+export function FullScreenMedia() {
+  const handleScroll = e => {
+    e.preventDefault()
+    window.scrollTo({
+      top: window.innerHeight - 64,
+      behavior: 'smooth'
+    })
+  }
+
+  return (
+    <div className='flex justify-center w-full h-full absolute top-0 overflow-hidden will-change-transform'>
+      <video
+        className='block object-cover w-full h-full pointer-events-none'
+        src='https://easyaistorageaccount.blob.core.windows.net/easyai/首页3.mp4'
+        loop
+        muted
+        playsInline
+        autoPlay
+      />
+      <a
+        onClick={handleScroll}
+        className='scroll-btn absolute text-white/70 bottom-8 z-20 cursor-pointer'>
+        ↓
+      </a>
+    </div>
+  )
+}
