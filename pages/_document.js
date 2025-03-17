9639b57886af84bd8ab5721bb5d96dceb9b64f32
lql <lin_passerby@163.com>
2025-03-17 15:39:47 +0800

    pwa配置3

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import BLOG from '@/blog.config'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import BLOG from "@/blog.config"
+import Document, { Head, Html, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,21 +11,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={BLOG.LANG}>
+        <link rel="manifest" href="/manifest.json" />
         <Head>
           {/* 预加载字体 */}
           {BLOG.FONT_AWESOME && (
             <>
               <link
-                rel='preload'
+                rel="preload"
                 href={BLOG.FONT_AWESOME}
-                as='style'
-                crossOrigin='anonymous'
+                as="style"
+                crossOrigin="anonymous"
               />
               <link
-                rel='stylesheet'
+                rel="stylesheet"
                 href={BLOG.FONT_AWESOME}
-                crossOrigin='anonymous'
-                referrerPolicy='no-referrer'
+                crossOrigin="anonymous"
+                referrerPolicy="no-referrer"
               />
             </>
           )}
