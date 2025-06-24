429d05b0df4d3d9870bc19d11b2531f4f587e55f
lql <lin_passerby@163.com>
2025-06-24 10:15:59 +0800

    添加备案号

--- blog.config.js
+++ blog.config.js
@@ -4,51 +4,51 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/1a0c05ba848e80329de5f922cff964cf
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    "1a0c05ba848e80329de5f922cff964cf,en:7c1d570661754c8fbc568e00a01fd70e",
-  THEME: process.env.NEXT_PUBLIC_THEME || "heoDiy", // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
-  LANG: process.env.NEXT_PUBLIC_LANG || "zh-CN", // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
+    '1a0c05ba848e80329de5f922cff964cf,en:7c1d570661754c8fbc568e00a01fd70e',
+  THEME: process.env.NEXT_PUBLIC_THEME || 'heoDiy', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+  LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2021, // e.g if leave this empty, current year will be used.
 
   PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
-  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || "light", // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
+  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || "NotionNext", // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || "一个普通的干饭人🍚", // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || "https://tangly1024.com", // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || "Passerby", // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || "/images/icon-192x192.png", // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || "", // 备案号 闽ICP备XXXXXX
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Passerby', // 网站关键词 英文逗号隔开
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/images/icon-192x192.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '渝ICP备2025062239号', // 备案号
   BEI_AN_LINK:
-    process.env.NEXT_PUBLIC_BEI_AN_LINK || "https://beian.miit.gov.cn/", // 备案查询链接，如果用了萌备等备案请在这里填写
+    process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
 
   // 其它复杂配置
   // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
-  ...require("./conf/comment.config"), // 评论插件
-  ...require("./conf/contact.config"), // 作者联系方式配置
-  ...require("./conf/post.config"), // 文章与列表配置
-  ...require("./conf/analytics.config"), // 站点访问统计
-  ...require("./conf/image.config"), // 网站图片相关配置
-  ...require("./conf/font.config"), // 网站字体
-  ...require("./conf/right-click-menu"), // 自定义右键菜单相关配置
-  ...require("./conf/code.config"), // 网站代码块样式
-  ...require("./conf/animation.config"), // 动效美化效果
-  ...require("./conf/widget.config"), // 悬浮在网页上的挂件，聊天客服、宠物挂件、音乐播放器等
-  ...require("./conf/ad.config"), // 广告营收插件
-  ...require("./conf/plugin.config"), // 其他第三方插件 algolia全文索引
+  ...require('./conf/comment.config'), // 评论插件
+  ...require('./conf/contact.config'), // 作者联系方式配置
+  ...require('./conf/post.config'), // 文章与列表配置
+  ...require('./conf/analytics.config'), // 站点访问统计
+  ...require('./conf/image.config'), // 网站图片相关配置
+  ...require('./conf/font.config'), // 网站字体
+  ...require('./conf/right-click-menu'), // 自定义右键菜单相关配置
+  ...require('./conf/code.config'), // 网站代码块样式
+  ...require('./conf/animation.config'), // 动效美化效果
+  ...require('./conf/widget.config'), // 悬浮在网页上的挂件，聊天客服、宠物挂件、音乐播放器等
+  ...require('./conf/ad.config'), // 广告营收插件
+  ...require('./conf/plugin.config'), // 其他第三方插件 algolia全文索引
 
   // 高级用法
-  ...require("./conf/layout-map.config"), // 路由与布局映射自定义，例如自定义特定路由的页面布局
-  ...require("./conf/notion.config"), // 读取notion数据库相关的扩展配置，例如自定义表头
-  ...require("./conf/dev.config"), // 开发、调试时需要关注的配置
+  ...require('./conf/layout-map.config'), // 路由与布局映射自定义，例如自定义特定路由的页面布局
+  ...require('./conf/notion.config'), // 读取notion数据库相关的扩展配置，例如自定义表头
+  ...require('./conf/dev.config'), // 开发、调试时需要关注的配置
 
   // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: [""], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: [""], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
+  CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
+  CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 自定义菜单
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
@@ -63,7 +63,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    "Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉",
+    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
--- themes/heoDiy/components/Footer.js
+++ themes/heoDiy/components/Footer.js
@@ -1,18 +1,19 @@
-import { BeiAnGongAn } from "@/components/BeiAnGongAn"
-import CopyRightDate from "@/components/CopyRightDate"
-import PoweredBy from "@/components/PoweredBy"
-import { siteConfig } from "@/lib/config"
-import SocialButton from "./SocialButton"
+import { BeiAnGongAn } from '@/components/BeiAnGongAn'
+import CopyRightDate from '@/components/CopyRightDate'
+import PoweredBy from '@/components/PoweredBy'
+import { siteConfig } from '@/lib/config'
+import SocialButton from './SocialButton'
 
 /**
  * 页脚
  * @returns
  */
 const Footer = () => {
-  const BEI_AN = siteConfig("BEI_AN")
-  const BIO = siteConfig("BIO")
+  const BEI_AN = siteConfig('BEI_AN')
+  const BIO = siteConfig('BIO')
   return (
-    <footer className="relative m-auto w-full flex-shrink-0 justify-center bg-white text-center text-sm leading-6 text-gray-600 dark:bg-[#1a191d] dark:text-gray-100">
+    <footer
+      className="relative m-auto w-full flex-shrink-0 justify-center bg-white text-center text-sm leading-6 text-gray-600 dark:bg-[#1a191d] dark:text-gray-100">
       {/* 颜色过度区 */}
       <div
         id="color-transition"
@@ -31,25 +32,25 @@ const Footer = () => {
         id="footer-bottom"
         className="flex h-20 w-full flex-col items-center justify-between border-t bg-[#f1f3f7] p-3 px-6 dark:border-t-[#3D3D3F] dark:bg-[#21232A] lg:flex-row">
         <div id="footer-bottom-left" className="text-center lg:text-start">
-          <PoweredBy />
+          {/*<PoweredBy />*/}
           <div className="flex gap-x-1">
             暂时还没有想好写点啥
-            {/*<CopyRightDate />*/}
+            <CopyRightDate />
             {/*<a*/}
             {/*  href={'/about'}*/}
-            {/*  className='underline font-semibold dark:text-gray-300 '>*/}
+            {/*  className="underline font-semibold dark:text-gray-300 ">*/}
             {/*  {siteConfig('AUTHOR')}*/}
             {/*</a>*/}
-            {/*{BIO && <span className='mx-1'> | {BIO}</span>}*/}
+            {BIO && <span className="mx-1"> | {BIO}</span>}
           </div>
         </div>
 
         <div id="footer-bottom-right">
           {BEI_AN && (
             <>
-              <i className="fas fa-shield-alt" />{" "}
+              <i className="fas fa-shield-alt" />{' '}
               <a href="https://beian.miit.gov.cn/" className="mr-2">
-                {siteConfig("BEI_AN")}
+                {siteConfig('BEI_AN')}
               </a>
             </>
           )}
@@ -57,14 +58,15 @@ const Footer = () => {
 
           <span className="busuanzi_container_site_pv hidden">
             <i className="fas fa-eye" />
-            <span className="busuanzi_value_site_pv px-1"> </span>{" "}
+            <span className="busuanzi_value_site_pv px-1"> </span>{' '}
           </span>
           <span className="busuanzi_container_site_uv hidden pl-2">
-            <i className="fas fa-users" />{" "}
-            <span className="busuanzi_value_site_uv px-1"> </span>{" "}
+            <i className="fas fa-users" />{' '}
+            <span className="busuanzi_value_site_uv px-1"> </span>{' '}
           </span>
 
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
+          {/*<h1*/}
+          {/*  className="text-xs pt-4 text-light-400 dark:text-gray-400">{siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1>*/}
         </div>
       </div>
     </footer>
